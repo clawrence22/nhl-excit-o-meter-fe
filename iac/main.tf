bdbe41445f9425e77425090f39cd4f57da3543bb
Clinton Lawrence <clinton.lawrence2@gmail.com>
2026-03-09 11:28:11 -0700

    no port

--- iac/main.tf
+++ iac/main.tf
@@ -198,7 +198,7 @@ resource "aws_ecs_task_definition" "app" {
         }
       }
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:5001/health || exit 1"]
+        command     = ["CMD-SHELL", "curl -f http://localhost/health || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
