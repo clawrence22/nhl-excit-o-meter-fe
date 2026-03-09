050a61c86117be45e536f9b101dc66892261d3ee
Clinton Lawrence <clinton.lawrence2@gmail.com>
2026-03-09 11:10:25 -0700

    health check

--- iac/main.tf
+++ iac/main.tf
@@ -197,6 +197,13 @@ resource "aws_ecs_task_definition" "app" {
           awslogs-stream-prefix = "ecs"
         }
       }
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -f http://localhost/health || exit 1"]
+        interval    = 30
+        timeout     = 5
+        retries     = 3
+        startPeriod = 60
+      }
     }
   ])
 }
