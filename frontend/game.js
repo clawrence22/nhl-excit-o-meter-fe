88383d77650a91e212d86bd8547ac27c1de1d7d5
Clinton Lawrence <clinton.lawrence2@gmail.com>
2026-03-11 15:49:43 -0700

    oops

--- frontend/game.js
+++ frontend/game.js
@@ -237,9 +237,17 @@ class GameAnalyzer {
        {
          badges.push({icon: '🚨', label: 'High-scoring Game', type: "highlight"});
        }
-       if (modifiers["ice-tilt"])
+       if (modifiers["chances_ice_tilt"])
        {
-         badges.push({icon: '⚖️', label: 'Ice Tilt', type: "detractor"});
+         badges.push({icon: '⚖️', label: 'Ice Tilt (Chances)', type: "detractor"});
+       }
+       if (modifiers["goals_ice_tilt"])
+       {
+         badges.push({icon: '⚖️', label: 'Ice Tilt (Goals)', type: "detractor"});
+       }
+       if (modifiers["xgoals_ice_tilt"])
+       {
+         badges.push({icon: '⚖️', label: 'Ice Tilt (xGoals)', type: "detractor"});
        }
        if (modifiers["next-goal-wins"])
        {
